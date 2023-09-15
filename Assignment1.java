6fee69c04bb886603a4d2f5f06a2819e893d63cc
jakebalula <balula@hartford.edu>
2023-09-15 19:16:49 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -4,5 +4,19 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println(); //Space added to make it look better on run
+    //Series of println to make block letters
+    System.out.println("   JJJJJJJJJJJJJJJ     BBBBBBBBBB");
+    System.out.println("   JJJJJJJJJJJJJJJ     BBBBBBBBBBBB");
+    System.out.println("           JJJJ        BBBB     BBB");
+    System.out.println("           JJJJ        BBBB     BBBB");
+    System.out.println("           JJJJ        BBBB     BBB");
+    System.out.println("           JJJJ        BBBBBBBBBB");
+    System.out.println("           JJJJ        BBBB     BBB");
+    System.out.println("   JJ      JJJJ        BBBB     BBBB");
+    System.out.println("    JJJ    JJJJ        BBBB     BBB");
+    System.out.println("     JJJJJJJJJJ        BBBBBBBBBBBB");
+    System.out.println("      JJJJJJJ          BBBBBBBBBB");
+    System.out.println(); //Space added to make it look better on run
   }
 }
