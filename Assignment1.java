85cfaf40301292a26a6c0f69836e81dbafd66a81
jakebalula <balula@hartford.edu>
2023-09-16 19:10:24 -0400

    Added code for step 2 of Assignment

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,6 @@
 // Import libraries here
 // Jacob Balula
-
+import java.util.Scanner;
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
@@ -18,5 +18,17 @@ public class Assignment1 {
     System.out.println("     JJJJJJJJJJ        BBBBBBBBBBBB");
     System.out.println("      JJJJJJJ          BBBBBBBBBB");
     System.out.println(); //Space added to make it look better on run
+    String number;
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Please enter a number in Farenheit:"); //Asks the user to enter a temp.
+      number = scan.nextLine();
+      System.out.println(number); //Prints out users response
+      final float Conversion = 5.0 / 9.0; // sets the conversion factor in order to get celsius
+      double celsiusTemp;
+      int finaltemp;
+      finaltemp = number * Conversion;
+      System.out.println(number + "in celsius is" + finaltemp);
+
+
   }
 }
