c7a5bcb6d8157642049bdcbf123ea0664485a9c1
jakebalula <balula@hartford.edu>
2023-09-16 19:38:03 -0400

    Updates to conversion code

--- Assignment1.java
+++ Assignment1.java
@@ -18,16 +18,13 @@ public class Assignment1 {
     System.out.println("     JJJJJJJJJJ        BBBBBBBBBBBB");
     System.out.println("      JJJJJJJ          BBBBBBBBBB");
     System.out.println(); //Space added to make it look better on run
-    String number;
-      Scanner scan = new Scanner(System.in);
-      System.out.println("Please enter a number in Farenheit:"); //Asks the user to enter a temp.
-      number = scan.nextLine();
-      System.out.println(number); //Prints out users response
-      final float Conversion = 5.0 / 9.0; // sets the conversion factor in order to get celsius
-      double celsiusTemp;
-      int finaltemp;
-      finaltemp = number * Conversion;
-      System.out.println(number + "in celsius is" + finaltemp);
+      Double number;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number in Farenheit:"); //Asks the user to enter a temp.
+            number = scan.nextDouble();
+            System.out.println(number); //Prints out users response
+            Double celsius = (5.0 / 9.0) * (number - 32);
+          System.out.println(number + " in celsius is " + celsius);
 
 
   }
