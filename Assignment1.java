ce2bfa731d8aa028a32b222292dcf461ab2c5296
jakebalula <balula@hartford.edu>
2023-09-18 14:24:58 -0400

    Simple updates

--- Assignment1.java
+++ Assignment1.java
@@ -26,13 +26,14 @@ public class Assignment1 {
             System.out.println(number); //Prints out users response
             Double celsius = (5.0 / 9.0) * (number - 32); // Converts users input to celsius
           System.out.println(number + " in celsius is " + celsius); //DELETE IN FINAL CODE
+
                 String reverse;
                 String change1, change2;
                 Scanner scan2 = new Scanner(System.in);
-                System.out.println("Please enter a 5-character string");
-                reverse = scan2.nextLine().substring(1 , 4);
-                change1 = reverse.substring(3, 2);
-                    System.out.println(change1);
+                  System.out.println("Please enter a 5-character string");
+                  reverse = scan2.nextLine().substring(1 , 4); //Removes first and last letter of users input
+                   /*change1 = reverse.replace( );
+                     System.out.println(change1);*/
               
   }
 }
